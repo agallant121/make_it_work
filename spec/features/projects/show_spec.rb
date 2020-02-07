0fad8a882d593009e003a26dc96c8d11c1e81df4
Alex Gallant <agallant1201@gmail.com>
2020-02-07 10:19:01 -0700

    User story 3 and helper method

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -3,7 +3,16 @@ require 'rails_helper'
 RSpec.describe "As a visitor" do
   before :each do
     @challenge_1 = Challenge.create(theme: "Old Stuff", project_budget: 500)
+    @challenge_2 = Challenge.create(theme: "Auto", project_budget: 1000)
+    @challenge_3 = Challenge.create(theme: "House", project_budget: 1500)
+
     @project_1 = @challenge_1.projects.create(name: "Garbage", material: "trash")
+    @project_2 = @challenge_2.projects.create(name: "Cars", material: "tires")
+    @project_3 = @challenge_3.projects.create(name: "Linens", material: "sheets")
+
+    @alex = @project_1.contestants.create(name: "Alex", age: 31, hometown: "Bangor", years_of_experience: 10)
+    @abby = @project_2.contestants.create(name: "Abby", age: 32, hometown: "Yarmouth", years_of_experience: 8)
+    @aila = @project_3.contestants.create(name: "Aila", age: 30, hometown: "Denver", years_of_experience: 9)
 
     visit "/projects/#{@project_1.id}"
   end
@@ -13,6 +22,12 @@ RSpec.describe "As a visitor" do
     expect(page).to have_content(@challenge_1.theme)
     expect(page).to have_content(@project_1.name)
     expect(page).to have_content(@project_1.material)
+  end
+
+  it "shows the number of contestants on each project" do
+    within("#project-#{@project_1.id}") do
+      expect(page).to have_content(1)
+    end
   end
 end
 
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  it "should count contestants per project" do
+     challenge_1 = Challenge.create(theme: "Old Stuff", project_budget: 500)
+     challenge_2 = Challenge.create(theme: "Auto", project_budget: 1000)
+     challenge_3 = Challenge.create(theme: "House", project_budget: 1500)
+
+     project_1 = challenge_1.projects.create(name: "Garbage", material: "trash")
+     project_2 = challenge_2.projects.create(name: "Cars", material: "tires")
+     project_3 = challenge_3.projects.create(name: "Linens", material: "sheets")
+
+     alex = project_1.contestants.create(name: "Alex", age: 31, hometown: "Bangor", years_of_experience: 10)
+     abby = project_2.contestants.create(name: "Abby", age: 32, hometown: "Yarmouth", years_of_experience: 8)
+     aila = project_3.contestants.create(name: "Aila", age: 30, hometown: "Denver", years_of_experience: 9)
+
+     expect(project_1.contestant_count). to eq(1)
+
+  end
 end
