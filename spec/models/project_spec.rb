a0baf9e8a920c57a7576d2a2143ebb5f62896a2e
Alex Gallant <agallant1201@gmail.com>
2020-02-07 11:19:15 -0700

    final push

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -13,20 +13,33 @@ RSpec.describe Project, type: :model do
     it {should have_many(:contestants).through(:contestant_projects)}
   end
 
-  it "should count contestants per project" do
-     challenge_1 = Challenge.create(theme: "Old Stuff", project_budget: 500)
-     challenge_2 = Challenge.create(theme: "Auto", project_budget: 1000)
-     challenge_3 = Challenge.create(theme: "House", project_budget: 1500)
+  describe "model methods" do
+    it "should count contestants per project" do
+       challenge_1 = Challenge.create(theme: "Old Stuff", project_budget: 500)
+       challenge_2 = Challenge.create(theme: "Auto", project_budget: 1000)
+       challenge_3 = Challenge.create(theme: "House", project_budget: 1500)
 
-     project_1 = challenge_1.projects.create(name: "Garbage", material: "trash")
-     project_2 = challenge_2.projects.create(name: "Cars", material: "tires")
-     project_3 = challenge_3.projects.create(name: "Linens", material: "sheets")
+       project_1 = challenge_1.projects.create(name: "Garbage", material: "trash")
+       project_2 = challenge_2.projects.create(name: "Cars", material: "tires")
+       project_3 = challenge_3.projects.create(name: "Linens", material: "sheets")
 
-     alex = project_1.contestants.create(name: "Alex", age: 31, hometown: "Bangor", years_of_experience: 10)
-     abby = project_2.contestants.create(name: "Abby", age: 32, hometown: "Yarmouth", years_of_experience: 8)
-     aila = project_3.contestants.create(name: "Aila", age: 30, hometown: "Denver", years_of_experience: 9)
+       alex = project_1.contestants.create(name: "Alex", age: 31, hometown: "Bangor", years_of_experience: 10)
+       abby = project_2.contestants.create(name: "Abby", age: 32, hometown: "Yarmouth", years_of_experience: 8)
+       aila = project_3.contestants.create(name: "Aila", age: 30, hometown: "Denver", years_of_experience: 9)
 
-     expect(project_1.contestant_count). to eq(1)
+       expect(project_1.contestant_count). to eq(1)
+    end
 
+    it "can use average experience method" do
+      challenge_4 = Challenge.create(theme: "School", project_budget: 2000)
+
+      project_4 = challenge_4.projects.create(name: "Schoolhouse", material: "books")
+
+      aaron = project_4.contestants.create(name: "Aaron", age: 34, hometown: "Bangor", years_of_experience: 10)
+      marlee = project_4.contestants.create(name: "Marlee", age: 33, hometown: "Yarmouth", years_of_experience: 8)
+      cheryl = project_4.contestants.create(name: "Cheryl", age: 33, hometown: "Denver", years_of_experience: 9)
+
+      expect(project_4.average_experience).to eq(9)
+    end
   end
 end
